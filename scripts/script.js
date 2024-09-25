5f677b468bc9bc620238754bfc5aa2e7f3d66dd0
sophiamuntinga <sophiamuntinga@gmail.com>
2024-09-25 16:54:09 +0200

    hamburger-attempt-verwijderd

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,4 @@
 // JavaScript Document
 console.log("hi");
 
-// Selecteer de button en nav-elementen
-const menuButton = document.querySelector('nav button');
-const nav = document.querySelector('nav');
 
-// Voeg een click event toe om het menu te tonen of te verbergen
-menuButton.addEventListener('click', function() {
-    nav.classList.toggle('active'); // Toggle de 'active' class
-});
